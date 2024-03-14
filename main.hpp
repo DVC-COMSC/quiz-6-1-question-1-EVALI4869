38f1186a3ef4296384032b2a17e712859f17fbc2
EVALI4868 <eeevalee4869@gmail.com>
2024-03-14 14:13:46 -0700

    quiz 6-1-1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,52 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+int getTwoValues(int, int);
+int getNextPrime(int);
+int getPrevPrime(int);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+int getTwoValues(int begin, int end)
+{
+    cin >> begin >> end;
+        if (begin > end){
+                return 1;
+        }
+        return 0;
+}
+
+int getNextPrime(int begin)
+{
+        int i, j, prime1;
+        for (i = begin; true; i++){
+                bool flag = 1;
+                for (j=2; j <=(i / 2); j++){
+                        if (i % j == 0){
+                                flag = 0;
+                        }
+                }
+                if (flag) {
+                                prime1 = i;
+                                break;
+                        }
+                }
+        return prime1;
+}
+
+int getPrevPrime(int end)
+{
+    int i, j, prime2;
+    for (i = end; true; i--){
+            bool flag = 1;
+            for (j=2; j <=(i / 2); j++){
+                    if (i % j == 0){
+                        flag = 0;
+                    }
+            }
+            if (flag) {
+                        prime2 = i;
+                        break;
+                    }
+    }
+        return prime2;
+
+}
